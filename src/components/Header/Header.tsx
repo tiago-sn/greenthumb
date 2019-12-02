b765a660f527c355a68f7432b75d8d12f1b15b62
Tiago <tiagoletras45@gmail.com>
2019-12-02 13:38:47 -0300

    Add a link to the homepage in the Header logo.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Logo from './assets/logo.svg';
 
 const Header: React.FC = () => {
-  return <header><img src={Logo} alt="Logo Green thumb" /></header>
+  return (
+    <header>
+      <Link to="/">
+        <img src={Logo} alt="Logo Green Thumb" />
+      </Link>
+    </header>);
 }
 
 export default Header;
