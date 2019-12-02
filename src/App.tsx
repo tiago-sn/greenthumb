02525948605e5b9821715ff74448a2e0bf91656b
Tiago <tiagoletras45@gmail.com>
2019-12-02 13:40:45 -0300

    Add react-redux Provider.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Home from './components/containers/Home';
 import Sunlight from './components/containers/Sunlight';
@@ -8,48 +9,52 @@ import Pets from './components/containers/Pets';
 import Results from './components/containers/Results';
 import Purchase from './components/containers/Purchase';
 
+import store from './store';
+
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home linkTo="/sunlight"/>
-          </Route>
-          
-          <Route path="/sunlight">
-            <Sunlight 
-              previous="/"
-              linkTo="/water"
-            />
-          </Route>
-          
-          <Route path="/water">
-            <Water 
-              previous="/sunlight"  
-              linkTo="/pets"
-            />
-          </Route>
-          
-          <Route path="/pets">
-            <Pets 
-              previous="/water" 
-              linkTo="/results"
-            />
-          </Route>
-          
-          <Route path="/results">
-            <Results linkTo="/purchase"/>
-          </Route>
-          
-          <Route path="/purchase">
-            <Purchase/>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home linkTo="/sunlight" />
+            </Route>
+
+            <Route path="/sunlight">
+              <Sunlight
+                previous="/"
+                linkTo="/water"
+              />
+            </Route>
+
+            <Route path="/water">
+              <Water
+                previous="/sunlight"
+                linkTo="/pets"
+              />
+            </Route>
+
+            <Route path="/pets">
+              <Pets
+                previous="/water"
+                linkTo="/results"
+              />
+            </Route>
+
+            <Route path="/results">
+              <Results linkTo="/purchase" />
+            </Route>
+
+            <Route path="/purchase">
+              <Purchase />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
