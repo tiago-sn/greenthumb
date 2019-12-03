1143e09e361d9b58550c3b08a534d99b8a52b5fa
Tiago <tiagoletras45@gmail.com>
2019-12-03 10:20:33 -0300

    Connect components to the store: Sunlight, Water, Pets.

--- src/components/containers/Sunlight/Sunlight.tsx
+++ src/components/containers/Sunlight/Sunlight.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+
+import { UserChoicesState } from '../../../store/types';
+import { userChoicesActionCreators } from '../../../store/actions';
+import { Sunlight as Sun, Water, Pets } from '../../../service/api/types';
 
 import Header from '../../Header';
 
-interface ISunlightProps {
+interface IStateProps {
+  sun: Sun;
+  water: Water;
+  pets: Pets;
+}
+
+interface IDispatchProps {
+  selectSunlight: typeof userChoicesActionCreators.selectSunlight;
+}
+
+interface IOwnProps {
   linkTo: string;
   previous: string;
 }
 
-const Sunlight: React.FC<ISunlightProps> = ({ linkTo, previous }) => {
+type ISunlightProps = IStateProps & IDispatchProps & IOwnProps;
+
+const Sunlight: React.FC<ISunlightProps> = ({ linkTo, previous, selectSunlight, sun }) => {
 
   return (
     <div>
       <Header />
       <h1>Sunlinght</h1>
+
+      <ul>
+        <li onClick={() => selectSunlight("high")}>high</li>
+        <li onClick={() => selectSunlight("low")}>low</li>
+        <li onClick={() => selectSunlight("no")}>no</li>
+      </ul>
+
+      <span>Selected: {sun}</span>
 
       <br />
 
@@ -28,4 +54,15 @@ const Sunlight: React.FC<ISunlightProps> = ({ linkTo, previous }) => {
   );
 }
 
-export default Sunlight;
+const mapStateToProps = (state: UserChoicesState) => ({
+  ...state
+});
+
+const mapDispatchToProps = (dispach: Dispatch) => {
+  return bindActionCreators(userChoicesActionCreators, dispach)
+}
+
+export default connect<IStateProps, IDispatchProps, IOwnProps, ISunlightProps>(
+  mapStateToProps,
+  mapDispatchToProps
+)(Sunlight);
