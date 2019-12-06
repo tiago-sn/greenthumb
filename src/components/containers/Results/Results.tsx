9b677dee869c0dd54653c51b51939019d50124ce
Tiago Santos <tiago@am3.localdomain>
2019-12-06 08:05:05 -0300

    Make the Results component able to dispatch the action to fetch the plant list.

--- src/components/containers/Results/Results.tsx
+++ src/components/containers/Results/Results.tsx
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Dispatch, bindActionCreators } from "redux";
 
 import Header from '../../Header';
 
-interface IResultsProps {
+import { Plant, Sunlight, Water, Pets } from "../../../service/api/types";
+import { ApplicationState } from "../../../store/types";
+import { ActionCreators } from "../../../store/actions";
+
+interface IStateProps {
+  plants: Plant[],
+  sun: Sunlight,
+  water: Water,
+  pets: Pets
+}
+
+interface IDispatchProps {
+  requestPlantList: typeof ActionCreators.requestPlantList
+}
+
+interface IOwnState {
   linkTo: string
 }
 
-const Results: React.FC<IResultsProps> = ({ linkTo }) => {
+type IResultsProps = IOwnState & IStateProps & IDispatchProps;
+
+const Results: React.FC<IResultsProps> = (
+  { linkTo, plants, sun, water, pets, requestPlantList }
+) => {
+
+  useEffect(() => {
+    requestPlantList({sun, water, pets})
+  }, [pets, requestPlantList, sun, water])
+
   return (
     <div>
       <Header />
+
+      <ul>
+        {
+          plants.length > 0 ? 
+          plants.map(plant => <li>{plant.name}</li>) : 
+          <span>Loading...</span>
+        }
+      </ul>
+
       <h1>Results</h1>
       <Link to={linkTo}>
         Next
@@ -19,4 +54,17 @@ const Results: React.FC<IResultsProps> = ({ linkTo }) => {
   );
 }
 
-export default Results;
+const mapStateToProps = (state: ApplicationState): IStateProps => ({
+  plants: state.plants,
+  sun: state.sun,
+  water: state.water,
+  pets: state.pets
+})
+
+const mapDispatchToProps = (dispatch: Dispatch): IDispatchProps =>
+  bindActionCreators(ActionCreators, dispatch);
+
+export default connect<IStateProps, IDispatchProps, IOwnState, ApplicationState>(
+  mapStateToProps,
+  mapDispatchToProps
+)(Results);
