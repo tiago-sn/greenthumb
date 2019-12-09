fa58911bf0c3e65e66a0e50548a630c8249cf46a
Tiago Santos <tiago@am3.localdomain>
2019-12-09 16:59:43 -0300

    Add <Button>, style.scss and backgound image to Home component.

--- src/components/containers/Home/Home .tsx
+++ src/components/containers/Home/Home .tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import Header from '../../Header';
+import Button from '../../Button';
+
+import "./style.scss";
+import bg from './img/bg.png';
+import rightArrowIcon from './img/right-arrow-icon.svg';
 
 interface IHomeProps {
   linkTo: string;
@@ -9,13 +12,20 @@ interface IHomeProps {
 
 const Home: React.FC<IHomeProps> = ({ linkTo }) => {
   return (
-    <div>
-      <Header />
-      <h1>Home</h1>
-      <Link to={linkTo}>
-        Next
-      </Link>
-    </div>
+    <main className="home">
+      <h1 className="title">Find your next green friend</h1>        
+      <Button 
+        className="button"
+        to={linkTo}
+        img={{
+          src: rightArrowIcon,
+          alt: "Right arrow"
+        }}
+      >
+        start quizz
+      </Button>
+      <img src={bg} alt="A digital draw of a person holding a plant and four other plants around."/>
+    </main>
   );
 }
 
