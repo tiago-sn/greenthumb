8b2a31ef03a9493b8cc32442c890ff2ce0178660
Tiago Santos <tiago@am3.localdomain>
2019-12-09 17:01:21 -0300

    Add <Header> to the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Water from './components/containers/Water';
 import Pets from './components/containers/Pets';
 import Results from './components/containers/Results';
 import Purchase from './components/containers/Purchase';
+import Header from './components/Header';
 
 import store from './store';
 
@@ -16,44 +17,45 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <div className="App">
         <Router>
-          <Switch>
-            <Route exact path="/">
-              <Home linkTo="/sunlight" />
-            </Route>
+          <div className="App">
+            <Header />
+              <Switch>
+                <Route exact path="/">
+                  <Home linkTo="/sunlight" />
+                </Route>
 
-            <Route path="/sunlight">
-              <Sunlight
-                previous="/"
-                linkTo="/water"
-              />
-            </Route>
+                <Route path="/sunlight">
+                  <Sunlight
+                    previous="/"
+                    linkTo="/water"
+                  />
+                </Route>
 
-            <Route path="/water">
-              <Water
-                previous="/sunlight"
-                linkTo="/pets"
-              />
-            </Route>
+                <Route path="/water">
+                  <Water
+                    previous="/sunlight"
+                    linkTo="/pets"
+                  />
+                </Route>
 
-            <Route path="/pets">
-              <Pets
-                previous="/water"
-                linkTo="/results"
-              />
-            </Route>
+                <Route path="/pets">
+                  <Pets
+                    previous="/water"
+                    linkTo="/results"
+                  />
+                </Route>
 
-            <Route path="/results">
-              <Results linkTo="/purchase" />
-            </Route>
+                <Route path="/results">
+                  <Results linkTo="/purchase" />
+                </Route>
 
-            <Route path="/purchase">
-              <Purchase />
-            </Route>
-          </Switch>
+                <Route path="/purchase">
+                  <Purchase />
+                </Route>
+              </Switch>
+          </div>
         </Router>
-      </div>
     </Provider>
   );
 }
